e8de51b8b280ffbeff3a194723aeaf05e05e639f
Rithika Guntu <rithikaguntu@Rithikas-MacBook-Air.local>
2025-08-03 12:45:49 -0400

    merged changes

--- calculator.py
+++ calculator.py
@@ -19,6 +19,10 @@ def sub(a,b):
     return a-b
 def mult(a,b):
     return a*b
+def div (a,b):
+    if a == 0:
+        raise ZeroDivisionError
+    return b/a
 def log(a,b):
     if a <= 0 or a == 1 or b <= 0:
         raise ValueError

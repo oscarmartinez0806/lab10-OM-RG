d6184f603d73dd93573b70e1e13edb41484c6567
oscarmartinez0806 <oscarm4656@gmail.com>
2025-08-03 12:27:24 -0400

    modified calculator

--- calculator.py
+++ calculator.py
@@ -6,6 +6,12 @@ calculator.py
 One function per operation, in order.
 """
 # First example
+def sqaure_root(a):
+    if a < 0:
+        raise ValueError
+    return math.sqrt(a)
+def hypotenuse(a, b):
+    return math.hypot(a, b)
 def add(a, b): return a + b
 def sub(a, b): return a - b
 def mul(a, b): return a * b
